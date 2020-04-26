b93cfd7ea357a0d1fb1ed5b7fd0ccc812c453300
Zhihao Zhang <zzh19910530@163.com>
2020-04-27 00:10:28 +0800

    slack message deleter

---
+++ README.md
@@ -0,0 +1,50 @@
+# Slack Message Deleter
+
+> Free slack account only could has 10000 messages, so it needed to delete unused messages frequently. So this tool apprears.
+
+## Configure .env
+
+### 1. rename `.env-example` to `.env`
+
+### 2. generate a token from slack
+
+### 3. Configure `.env`
+
+> If you have multiple channels which want to deleted, please use `,` to separate channels
+
+```bash
+TOKEN=
+CHANNEL_ID=
+```
+
+### Install node modules
+
+```bash
+yarn
+```
+
+## 🚀 Delete Slack Message
+
+> Delete messages except pinned or stared messages
+
+```bash
+node delete-slack-message.js
+```
+
+- delete a message per 100 ms to throttle the Slack API usage to avoid request failed(You could change the frequency)
+
+## Author
+
+👤 **Zhihao Zhang**
+
+- Github: [@zzh1991](https://github.com/zzh1991)
+
+## Show your support
+
+Please ⭐️ this repository if this project helped you!
+
+## 📝 License
+
+Copyright © 2020 [zzh1991](https://github.com/zzh1991).<br />
+This project is [MIT](https://github.com/zzh1991/slack-message-deleter/blob/master/LICENSE) licensed.
+
---
+++ delete-slack-message.js
@@ -0,0 +1,72 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+const sleep = require('sleep');
+
+dotenv.config();
+
+const BASE_API_URL = 'https://slack.com/api/';
+const TOKEN = process.env.TOKEN;
+const CHANNEL = process.env.CHANNEL_ID;
+
+const getHistoryApiUrl = (channel) => {
+  return `${BASE_API_URL}conversations.history?token=${TOKEN}&count=100&channel=${channel}&cursor=`;
+};
+
+const getDeleteApiUrl = (channel) => {
+  return `${BASE_API_URL}chat.delete?token=${TOKEN}&channel=${channel}&ts=`;
+};
+
+const isNullorUndefined = (item) => {
+  return item === null || item === undefined;
+};
+
+const getMessageList = async(channel) => {
+  try {
+    const messageList = [];
+    const data = await axios.get(getHistoryApiUrl(channel), {
+      timeout: 30000,
+    });
+    data.data.messages.forEach(message => {
+      if (isNullorUndefined(message.is_starred || null)
+        && isNullorUndefined(message.pinned_to || null)) {
+          messageList.push(message.ts);
+      }
+    });
+    return messageList;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+const deleteMessage = async(channel, message) => {
+  try {
+    await sleep.msleep(100);
+    await axios.get(getDeleteApiUrl(channel).concat(message), {
+      timeout: 5000,
+    });
+    console.log('delete message', message);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const getDataThenDelete = async() => {
+  let messageList = [];
+  const channelList = CHANNEL.split(',');
+  for (const channel of channelList) {
+    console.log('start to delete messages from', channel);
+    messageList = await getMessageList(channel);
+    console.log('message list size is', messageList.length);
+    while (messageList.length !== 0) {
+      for (const message of messageList) {
+        await deleteMessage(CHANNEL, message);
+      }
+      messageList = await getMessageList(channel);
+      console.log('message list size is', messageList.length);
+    }
+    console.log('messages deleted from', channel);
+  }
+}
+
+getDataThenDelete();
